e4fdafcd0403c3952696f2920606d7725c73d8c6
Isaac Kiyimba <isaaceduardk@gmail.com>
2020-11-11 01:24:43 +0300

    Finalizing project setup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import DecisionApp from "./components/DecisionApp"
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
 
-ReactDOM.render(<DecisionApp/>,
+ReactDOM.render(<p>Bolerplate App</p>,
     document.getElementById("app")
 ) 
